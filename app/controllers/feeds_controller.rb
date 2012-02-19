c91c175c9e4fe9f1d8506e28142cf92fd9e88ea5
GuoLei <guolei9@gmail.com>
2012-02-19 22:17:45 +0800

    feed list

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,6 +1,8 @@
 class FeedsController < ApplicationController
-  before_filter :authenticate_user!, :except=>:create
+  #before_filter :authenticate_user!, :except=>:create
   def index
-    @feeds = current_user.feeds.all(:limit=>10)
+    #@feeds = current_user.feeds.all(:limit=>10)
+    Feed.create(:event => "Hello hello")
+    render :json => Feed.all.to_json
   end
 end
--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,3 +1,15 @@
 class Feed < ActiveRecord::Base
   belongs_to :user
+  
+  def as_json(options = {})
+    super({:only => [:id], :methods => [:content, :createdAt]})
+  end
+  
+  def content
+    self.event
+  end
+  
+  def createdAt
+    self.created_at.to_s(:db)
+  end
 end
