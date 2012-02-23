252b5332e3f9cdff7928c07822056e94eb0998e7
GuoLei <guolei9@gmail.com>
2012-02-23 22:01:21 +0800

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,9 +6,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+[User, Care, AppList, App, Feed].each{|t| t.delete_all}
+
 al = AppList.create(:name => "Weather", :description => "Get notified of extreme weather conditions", :url=>"remote-weather")
 
-[User, Care, AppList, App, Feed].each{|t| t.delete_all}
 user = User.create(:email => "guolei@gmail.com", :password => "111111", :password_confirmation => "111111", :authentication_token => "sSCqFfknVDzP7FnyQrwq")
 care = Care.create!(:name => "Mother", :phone_number => "13800138000", :owner => user)
 
